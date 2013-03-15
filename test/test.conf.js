6a4acc2ac449825e929b0158f542f85f622063a8
Dean Sofer <dsofer@paxata.com>
2013-03-15 13:20:36 -0700

    fix(test): Removed angular-module component

---
+++ test/test.conf.js
@@ -0,0 +1,22 @@
+basePath = '../..';
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'components/jquery/jquery.js',
+  'components/angular/angular.js',
+  'components/*/*.js',
+  'src/*.js',
+  'test/*Spec.js'
+];
+
+// Avoid including minified version of angular and other libs again
+exclude = [
+  'components/*/*.min.js'
+];
+
+singleRun = true;
+
+reporters = [
+	'dots'
+];
