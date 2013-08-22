bfbf2c76218b2a897282f69f165513dc5dfef1f9
markcooper <mark.v.cooper@gmail.com>
2013-08-22 12:22:15 -0400

    Fix for IE8 splitting strings with forEach
    In IE8 when convertToSelect2Model is called on a String the forEach will split on each character. This fix prevents that.

--- src/select2.js
+++ src/select2.js
@@ -60,6 +60,7 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
 
           if (opts.simple_tags) {
             model = [];
+            angular_data = typeof angular_data == "string" && angular_data.indexOf(",") > -1 ? angular_data.split(",") : [angular_data]
             angular.forEach(
               angular_data,
               function(value, index) {
