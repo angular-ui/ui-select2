ff3ce10e4cbd37e4c10583404b7176d51db9cede
Dean Sofer <proloser@hotmail.com>
2013-11-22 16:20:25 -0800

    fix(backcompat): Adding a check for controller.$setPristine before calling

--- src/select2.js
+++ src/select2.js
@@ -121,7 +121,7 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
                 elm.select2('val', controller.$viewValue);
                 // Refresh angular to remove the superfluous option
                 elm.trigger('change');
-                if(newVal && !oldVal) {
+                if(newVal && !oldVal && controller.$setPristine) {
                   controller.$setPristine(true);
                 }
               });
