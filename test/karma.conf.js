e431a1c29a78c4a487ca12a860d362807d08adb7
lkptrzk <lkptrzk@gmail.com>
2014-03-20 13:54:32 -0400

    fix jquery bower reference in karma conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // Dependencies
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/select2/select2.js',
