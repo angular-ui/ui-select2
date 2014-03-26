6aa188ea7967f07711c2a236f1ca49a2cbb023cc
dmunch <dmunch@lucca.fr>
2014-03-26 17:27:02 +0100

    #12 Fix for sorting select element

--- src/select2.js
+++ src/select2.js
@@ -17,6 +17,7 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
       var watch,
         repeatOption,
         repeatAttr,
+        init = false,
         isSelect = tElm.is('select'),
         isMultiple = angular.isDefined(tAttrs.multiple);
 
@@ -93,7 +94,12 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
           }, true);
           controller.$render = function () {
             if (isSelect) {
-              elm.select2('val', controller.$viewValue);
+                $timeout(function() {
+                    if(!init) {
+                        elm.select2('val', controller.$viewValue);
+                        init = controller.$viewValue != undefined;
+                    }
+                });
             } else {
               if (opts.multiple) {
                 var viewValue = controller.$viewValue;
