4f06595db5cbd7a8ae8f5c43301ed88cc22a8866
Milos Mosovsky <milos.mosovsky@gmail.com>
2014-05-21 20:24:48 +0200

    Fix Jquery

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // Dependencies
-      'bower_components/jquery/jquery.js',
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
